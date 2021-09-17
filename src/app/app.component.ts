5c46bd74e27cdcb64b49a6a287c405015b63f769
Tiago <tiagolpadua@bb.com.br>
2021-09-17 07:48:21 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface City {
+  name: string,
+  code: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'droptest';
+
+  cities: City[];
+
+  selectedCity: City;
+
+  constructor() {
+      this.cities = [
+          {name: 'New York', code: 'NY'},
+          {name: 'Rome', code: 'RM'},
+          {name: 'London', code: 'LDN'},
+          {name: 'Istanbul', code: 'IST'},
+          {name: 'Paris', code: 'PRS'}
+      ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
